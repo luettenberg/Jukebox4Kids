6a8b1c51e0afc98a9f5bc36204fd2d077730f7ce
Andreas Mülder <A.Muelder@googlemail.com>
2017-11-09 19:44:55 +0100

    added buttons

--- connect.py
+++ connect.py
@@ -4,9 +4,17 @@
 # IMPORTS
 import sys
 import pprint
+import RPi.GPIO as GPIO
+import time
+
 
 from mpd import (MPDClient, CommandError)
 from socket import error as SocketError
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 HOST = 'localhost'
 PORT = '6600'
@@ -93,13 +101,27 @@ def main():
     printState(pp,client)
     
     listPlaylists(pp,client)
-    #loadAndPlayPlaylist(client, 'RITS Favs (by elixir046)')
-    playTrack(client, 'spotify:track:1ocmRsEMI6nO9d9BdQbXNI')
+    loadAndPlayPlaylist(client, 'RITS Favs (by elixir046)')
+    #playTrack(client, 'spotify:track:1ocmRsEMI6nO9d9BdQbXNI')
 
     ##client.playlistadd('kaffehausmusik','spotify:user:spotify:playlist:37i9dQZF1DX6KItbiYYmAv')
   
-    client.disconnect()
-    sys.exit(0)
+    while True:
+        play_ = GPIO.input(17)
+        prev_ = GPIO.input(22)
+        next_ = GPIO.input(27)
+        
+        if play_ == False:
+            print('Play Pressed')
+            client.pause()
+            
+        else if prev__ == False:
+            print('Prev Pressed')
+            client.previous()
+            
+        else if prev__ == False:
+            print('Next Pressed')
+            client.next()
 
 # Script starts here
 if __name__ == "__main__":
